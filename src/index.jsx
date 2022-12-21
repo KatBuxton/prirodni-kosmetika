dda05d4b014a71835b74ca5780191a3e37c3d25d
Katerina Tuckova <tuckova.mail@gmail.com>
2022-12-21 19:08:40 +0100

    fix1 defining cartItems with || []

--- src/index.jsx
+++ src/index.jsx
@@ -14,7 +14,7 @@ const App = () => {
   const [filteredItems, setFilteredItems] = useState(content);
   const [category, setCategory] = useState('');
   const [cartItems, setCartItems] = useState(
-    JSON.parse(localStorage.getItem('cartItems')),
+    JSON.parse(localStorage.getItem('cartItems') || []),
   );
   const [itemQuantity, setItemQuantity] = useState(1);
 
